1b656dfe1087919cda8c95c0dd0b26212d87d3d8
jipark96 <wlgus996@naver.com>
2022-07-27 18:21:56 +0900

    java 연습

---
+++ Chapter5/Student.java
@@ -0,0 +1,13 @@
+package Chapter5;
+
+public class Student {
+  int studentID;
+  String studentName;
+  int grade;
+  String address;
+
+  public void showStudentInfor() {
+    System.out.println(studentName + "," +address);
+  }
+
+}
---
+++ Chapter5/StudentTest.java
@@ -0,0 +1,12 @@
+package Chapter5;
+
+public class StudentTest {
+  
+  public static void main(String[] args) {
+
+    Student studentPark = new Student();
+    studentPark.studentName="박지현";
+    studentPark.address="서울";
+    studentPark.showStudentInfor();
+  }
+}
