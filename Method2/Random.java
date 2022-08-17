282451a6bcce95766a6592ab650e1db3b10a0f7e
jipark96 <wlgus996@naver.com>
2022-08-17 16:39:24 +0900

    메소드 끝 조건문 하는중

---
+++ Method2/Random.java
@@ -0,0 +1,20 @@
+//파라미터 값이 없는 메소드
+package Method2;
+
+public class Random {
+  public static void main(String[] args) {
+    // 메소드로부터 값을 반환
+    int x = rollDie();
+    
+    // 결과 출력
+    System.out.printf("주사위의 눈: %d", x);
+  }
+  
+  // 1~6 사이의 임의의 정수를 반환
+  public static int rollDie() {
+    double r = 6 * Math.random(); // 0.00..~ 5.99..
+    int temp = (int) r; // 0 ~ 5
+    int n = temp + 1; // 1 ~ 6
+    return n;
+  }
+}
